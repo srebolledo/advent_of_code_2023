75fe9f819824a5254ba8caad96f8ff6945250c04
Stephan Rebolledo <stephan.rebolledo@playstudios.com>
2023-12-22 20:30:14 -0300

    Day four added.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from day_one.main import solutions as day_one_solutions
 from day_two.main import solutions as day_two_solutions
 from day_three.main import solutions as day_three_solutions
+from day_four.main import solutions as day_four_solutions
 
 day_one_solutions()
 day_two_solutions()
 day_three_solutions()
+day_four_solutions()
